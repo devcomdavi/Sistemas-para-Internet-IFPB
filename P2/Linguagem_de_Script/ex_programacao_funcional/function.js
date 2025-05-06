a2ab6e5fd7a6acbcac6bca46ae04f71fe2599e4a
devcomdavi <davihmn6@gmail.com>
2025-05-06 17:28:21 -0300

    Uso de programação funcional em JavaScript

--- P2/Linguagem_de_Script/ex_programacao_funcional/function.js
+++ P2/Linguagem_de_Script/ex_programacao_funcional/function.js
@@ -1,15 +1,17 @@
-export function sum(array) {
+function sum(array) {
     return array.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0)
 }
 
-export function product(array) {
+function product(array) {
     const produto = array.reduce((acc, num) => acc * num, 1)
     return produto
 }
 
-export function sumOdds(array) {
+function sumOdds(array) {
     const somaImpares = array
         .filter(num => num%2 !== 0)
         .reduce((soma, num) => soma + num, 0)
     return somaImpares
 }
+
+export { sum, product, sumOdds } 
--- P2/Linguagem_de_Script/ex_programacao_funcional/main.js
+++ P2/Linguagem_de_Script/ex_programacao_funcional/main.js
@@ -1,31 +1,19 @@
 import { sum, product, sumOdds } from './function.js';
 
 let valores = [1, 2, 3];
+let valores2 = [2, 2, 2];
+let valores3 = [1, 2, 3, 4, 5, 6];
 console.log('sum');
-console.log(sum(valores));
+console.log('Valores: [1, 2, 3] -> ', sum(valores));
+console.log('Valores: [2, 2, 2] -> ', sum(valores2));
+console.log('Valores: [1, 2, 3, 4, 5, 6] -> ', sum(valores3));
 
-console.log('sumOdds');
-console.log(sumOdds(valores));
+console.log('\nsumOdds');
+console.log('Valores: [1, 2, 3] -> ', sumOdds(valores));
+console.log('Valores: [2, 2, 2] -> ', sumOdds(valores2));
+console.log('Valores: [1, 2, 3, 4, 5, 6] -> ', sumOdds(valores3));
 
-console.log('product');
-console.log(product(valores));
-
-valores = [2, 2, 2];
-console.log('sum');
-console.log(sum(valores));
-
-console.log('sumOdds');
-console.log(sumOdds(valores));
-
-console.log('product');
-console.log(product(valores));
-
-valores = [1, 2, 3, 4, 5, 6];
-console.log('sum');
-console.log(sum(valores));
-
-console.log('sumOdds');
-console.log(sumOdds(valores));
-
-console.log('product');
-console.log(product(valores));
+console.log('\nproduct');
+console.log('Valores: [1, 2, 3] -> ', product(valores));
+console.log('Valores: [2, 2, 2] -> ', product(valores2));
+console.log('Valores: [1, 2, 3, 4, 5, 6] -> ', product(valores3));
